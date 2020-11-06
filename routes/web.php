e2a67fcbb808379f8f26c17360c9063f7874564d
Ferdian Maulana <ferdi.maulana@gmail.com>
2020-11-06 17:08:40 +0700

    middleware auth

--- routes/web.php
+++ routes/web.php
@@ -9,12 +9,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/', 'DataController@index')->name('data.index');
 Route::post('/', 'DataController@store')->name('data.store');
 
-Route::prefix('dashboard')->group(function(){
-	Route::get('/', 'DataController@dashboard')->name('data.dashboard');
-	Route::prefix('data')->group(function(){
-		Route::get('edit/{id}', 'DataController@edit')->name('data.edit');
-		Route::put('update/{id}', 'DataController@update')->name('data.update');
-		Route::delete('delete/{id}', 'DataController@delete')->name('data.delete');
-		Route::get('datatable', 'DataController@datatable')->name('data.dt');
+Route::middleware('auth')->group(function(){
+	Route::prefix('dashboard')->group(function(){
+		Route::get('/', 'DataController@dashboard')->name('data.dashboard');
+		Route::prefix('data')->group(function(){
+			Route::get('edit/{id}', 'DataController@edit')->name('data.edit');
+			Route::put('update/{id}', 'DataController@update')->name('data.update');
+			Route::delete('delete/{id}', 'DataController@delete')->name('data.delete');
+			Route::get('datatable', 'DataController@datatable')->name('data.dt');
+		});
 	});
 });
